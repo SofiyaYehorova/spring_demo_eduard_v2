30d735041f3b08577f242a3b37ca1de50c1b1b2d
SofiyaYehorova <yehorova66@gmail.com>
2023-12-14 15:30:15 +0200

    basic

---
+++ src/main/java/org/example/spring_demo_eduard_v2/SpringDemoEduardV2Application.java
@@ -0,0 +1,13 @@
+package org.example.spring_demo_eduard_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoEduardV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoEduardV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_demo_eduard_v2/SpringDemoEduardV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_demo_eduard_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoEduardV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
